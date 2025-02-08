b5bd9449f149d88e7b6ac96af42918de3957cf78
ISHAAN NENE <ishaannene@gmail.com>
2025-02-08 08:48:29 +0530

    Updates

--- FrontEnd/src/pages/Login.tsx
+++ FrontEnd/src/pages/Login.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import { useUser } from '../contexts/UserContext';
+import { useUser } from '../context/UserContext';
 
 const Login = () => {
   const [formData, setFormData] = useState({
